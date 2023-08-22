ef83acf9226db6a2b8fd52f61f65c3b8fcd43b90
phylix <alirezarastegar117390@gmail.com>
2023-08-22 15:49:11 +0330

    add revoke tokens method to this class To expire all previous tokens

--- src/main/java/com/alireza/security/auth/AuthenticationService.java
+++ src/main/java/com/alireza/security/auth/AuthenticationService.java
@@ -1,12 +1,16 @@
 package com.alireza.security.auth;
 
 
+import com.alireza.security.entity.Token;
+import com.alireza.security.entity.User;
+import com.alireza.security.entity.enumeration.TokenType;
 import com.alireza.security.jwt.JwtService;
 import com.alireza.security.dto.auth.AuthenticationRequest;
 import com.alireza.security.dto.auth.AuthenticationResponse;
 import com.alireza.security.dto.auth.RegisterRequest;
 import com.alireza.security.entity.enumeration.Role;
 import com.alireza.security.mappers.AuthMapperImpl;
+import com.alireza.security.repository.TokenRepository;
 import com.alireza.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -24,18 +28,21 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final TokenRepository tokenRepository;
 
     public AuthenticationResponse register(RegisterRequest request) {
         var user = authMapper.registerRequestDtoToUser(request);
         user.setPassword(passwordEncoder.encode(request.getPassword()));
         user.setRole(Role.USER);
-        userRepository.save(user);
+        var savedUser = userRepository.save(user);
+
         var jwtToken = jwtService.generateToken(user);
+        saveToken(savedUser, jwtToken);
+
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
     }
-
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
         authenticationManager.authenticate(
@@ -45,9 +52,36 @@ public class AuthenticationService {
         var user = userRepository.findByEmail(request.getEmail()).orElseThrow(
                 () -> new UsernameNotFoundException("no user found with email")
         );
+
         var jwtToken = jwtService.generateToken(user);
+        revokeAllUserTokens(user);
+        saveToken(user, jwtToken);
+
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
+    }
+
+    private void revokeAllUserTokens(User user) {
+        var validUserTokens = tokenRepository.findAllValidTokensById(user.getId());
+        if(validUserTokens.isEmpty()) {
+            return;
+        }
+        validUserTokens.forEach(t -> {
+            t.setExpired(true);
+            t.setRevoked(true);
+        });
+        tokenRepository.saveAll(validUserTokens);
+    }
+
+    private void saveToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .expired(false)
+                .revoked(false)
+                .build();
+        tokenRepository.save(token);
     }
 }
