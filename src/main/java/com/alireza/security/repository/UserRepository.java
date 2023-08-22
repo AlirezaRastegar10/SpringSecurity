55f500151fc3a509db1641e909a7db5bc9221827
phylix <alirezarastegar117390@gmail.com>
2023-08-22 09:41:52 +0330

    create UserRepository interface as data access layer

---
+++ src/main/java/com/alireza/security/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.alireza.security.repository;
+
+import com.alireza.security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
