1290756714c4fd7a0342a376d1f04159ca79564f
phylix <alirezarastegar117390@gmail.com>
2023-08-31 01:42:32 +0330

    add delete endpoint for testing

--- src/main/java/com/alireza/security/controller/UserController.java
+++ src/main/java/com/alireza/security/controller/UserController.java
@@ -5,15 +5,15 @@ import com.alireza.security.dto.user.FindAllUserDto;
 import com.alireza.security.service.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class UserController {
 
     private final UserServiceImpl userService;
@@ -21,5 +21,11 @@ public class UserController {
     @GetMapping("/find-all")
     public ResponseEntity<List<FindAllUserDto>> findAll() {
         return ResponseEntity.ok(userService.findAll());
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN_DELETE')")
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable Long id) {
+        userService.delete(id);
     }
 }
