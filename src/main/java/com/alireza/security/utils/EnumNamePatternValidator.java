5c5de51b5ff030380abfd0c508d3dfd85e39cdfe
phylix <alirezarastegar117390@gmail.com>
2023-08-31 01:40:25 +0330

    implement role validation pattern in this class

---
+++ src/main/java/com/alireza/security/utils/EnumNamePatternValidator.java
@@ -0,0 +1,35 @@
+package com.alireza.security.utils;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+
+@Component
+public class EnumNamePatternValidator implements ConstraintValidator<RoleValidation, Enum<?>> {
+
+    private Pattern pattern;
+
+    @Override
+    public void initialize(RoleValidation annotation) {
+        try {
+            pattern = Pattern.compile(annotation.regexp());
+        } catch (PatternSyntaxException e) {
+            throw new IllegalArgumentException("Given regex is invalid", e);
+        }
+    }
+
+    @Override
+    public boolean isValid(Enum<?> value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return false;
+        }
+
+        Matcher m = pattern.matcher(value.name());
+        return m.matches();
+    }
+}
